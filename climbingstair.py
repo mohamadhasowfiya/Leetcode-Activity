0543402e985eb676c3036555a8211dc10e3b14ba
mohamadhasowfiya <mohamadhasowfiya786@gmail.com>
2026-04-08 17:57:51 +0530

    Create climbingstair.py

---
+++ climbingstair.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if n<=2:
+            return n
+        a,b=1,2
+        for i in range (3,n+1):
+            c=a+b
+            a=b
+            b=c
+        return b
+        
